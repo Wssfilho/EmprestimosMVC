0b75a54a4f5dc431efa42d13d3bd95ede388a6d6
Willzera <113561581+Wssfilho@users.noreply.github.com>
2025-04-17 18:03:49 -0300

    ADD: Funcionalidade Editar

---
+++ LivrosEmprestimos/Controllers/EmprestimoController.cs
@@ -0,0 +1,63 @@
+﻿using System.Linq;
+using LivrosEmprestimos.Data;
+using LivrosEmprestimos.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LivrosEmprestimos.Controllers
+{
+    public class EmprestimoController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public EmprestimoController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<EmprestimosModel> emprestimos = _context.Emprestimos;
+            return View(emprestimos);
+        }
+
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Editar(int? id)
+        {
+            if (id == 0 || id == null)
+            {
+                return NotFound();
+            }
+            EmprestimosModel emprestimos = _context.Emprestimos.FirstOrDefault(x => x.Id == id);
+            if(emprestimos == null)
+            {
+                return NotFound();
+            }
+
+            return View(emprestimos);
+        }
+        [HttpPost]
+        public IActionResult Cadastrar(EmprestimosModel emprestimos)
+        {
+            if(ModelState.IsValid)
+            {
+                _context.Emprestimos.Add(emprestimos);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Editar(EmprestimosModel emprestimo)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Emprestimos.Update(emprestimo);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(emprestimo);
+        }
+    }
+}
