1ca15e77d9f6facf41ddd8daf0bdb2de65ba77f0
Willzera <113561581+Wssfilho@users.noreply.github.com>
2025-04-17 22:14:28 -0300

    REFACTOR: Implementação do repositório de empréstimos e injeção de dependência

---
+++ Repository/Contracts/IEmprestimoRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace LivrosEmprestimos.Repository.Contracts;
+using LivrosEmprestimos.Models;
+using Microsoft.AspNetCore.Mvc;
+
+public interface IEmprestimoRepository
+    {
+    public EmprestimosModel Cadastrar(EmprestimosModel emprestimos);
+    public EmprestimosModel GetId(int? id);
+    public EmprestimosModel Editar(EmprestimosModel emprestimos);
+    public EmprestimosModel Excluir(EmprestimosModel emprestimos);
+    public IEnumerable<EmprestimosModel> GetAll();
+}
+    
+   
